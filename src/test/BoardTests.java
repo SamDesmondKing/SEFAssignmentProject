acb6911b4b9cddb298eb7e99fbf4319f899f8ebf
Sam King <samuel-king@live.com.au>
2019-09-14 16:22:34 +1000

    Updated Quazi's latest code with latest test files

--- src/test/BoardTests.java
+++ src/test/BoardTests.java
@@ -8,6 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import exceptions.SnakePlacementException;
+import main.Controller.GraphicsController;
 import main.Controller.SnakeController;
 import main.Model.Board;
 import main.Model.Dice;
@@ -25,10 +26,12 @@ public class BoardTests {
 		assertNotNull(board);
 	}
 	
+	
 	@Test
 	//check if dice exists
 	public void test2() {
 		Board board = new Board();
+		GraphicsController g = new GraphicsController(board);
 		assertThat(board.getDice(),instanceOf(Dice.class));
 	}
 	/*
@@ -39,5 +42,12 @@ public class BoardTests {
 	}
 	*/
 	
-	
+	@Test
+	//Mashrur test parts
+	public void test3() {
+		//test piece  location
+		Board b = new Board();
+		b.setPiece(b.getPieces()[0], 50);
+		assertEquals(b.getPieces()[0].getLocation(), 50);
+	}
 }
---
+++ src/test/SnakeControllerTests.java
@@ -0,0 +1,183 @@
+package test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import exceptions.SnakePlacementException;
+import main.Controller.BoardController;
+import main.Controller.SnakeController;
+import main.Model.Board;
+import main.Model.Dice;
+import main.Model.Ladder;
+import main.Model.Snake;
+import main.Model.SnakeGuard;
+
+public class SnakeControllerTests {
+	
+	
+	//Check SnakeController.move() to SnakeGuard location
+	@Test (expected = SnakePlacementException.class)
+	public void test1() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s = new Snake(24,18);
+		SnakeGuard sg = new SnakeGuard(25);
+		
+		//Adding entities
+		try {
+			bc.add(s);
+			bc.add(sg);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		//Moving Snake to SnakeGuard location
+		//Expecting SnakePlacementException
+		sc.move(s, 25);	
+	}
+	
+	
+	//Check SnakeController.move(snake) to location 
+	//which already has a snake head 
+	@Test (expected = SnakePlacementException.class)
+	public void test2() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(24,18);
+		Snake s2 = new Snake(20,10);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+			bc.add(s2);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		//Moving snake to location of
+		//other snake's head.
+		sc.move(s1, 20);
+	}
+	
+	//Testing exception thrown when snake moves to location of ladder head
+	@Test (expected = SnakePlacementException.class)
+	public void test3() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(24,18);
+		Ladder l1 = new Ladder(10,20);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+			bc.add(l1);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		//Moving snake to location of
+		//ladder head.
+		sc.move(s1, 20);
+	}
+	
+	//Testing exception thrown when snake moves to location of ladder bottom
+	@Test (expected = SnakePlacementException.class)
+	public void test4() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(24,18);
+		Ladder l1 = new Ladder(10,20);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+			bc.add(l1);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		//Moving snake to location of
+		//ladder bottom.
+		sc.move(s1, 10);	
+	}
+	
+			
+	//Checking exception thrown when tail would go below zero
+	@Test (expected = SnakePlacementException.class)
+	public void test5() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(20,1);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		//Moving snake such that tail would go below zero.
+		sc.move(s1, 10);
+	}		
+	
+	
+	@Test
+	//Checking SnakeController.getTarget() returns correct target when given direction
+	public void test6() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(20,1);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		assertEquals(sc.getTarget(s1,1), 21);
+	}
+	
+	@Test (expected = SnakePlacementException.class)
+	//Checking SnakeController.getTarget() 
+	//throws exception when given invalid direction
+	public void test7() throws SnakePlacementException {
+		
+		//Initializing controllers
+		Board b = new Board();
+		BoardController  bc = new BoardController(b);
+		SnakeController sc = new SnakeController(b);
+		Snake s1 = new Snake(20,1);
+		
+		//Adding entities
+		try {
+			bc.add(s1);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		sc.getTarget(s1,3);
+	}
+}
